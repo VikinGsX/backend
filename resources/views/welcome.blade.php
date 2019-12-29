37619c0253acd5ebf8de62da1fb236329f56324a
vagrant <vagrant@homestead>
2019-12-29 08:08:44 +0000

    dev12.29/modify welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>監視你的IP</title>
+        <title>ttxtt.xyz</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    系統正嘗試紀錄你的IP...
                 </div>
 
                 <div class="links">
